5c428dca640967a30c70077922e546214167f791
huangyuheng <1730969518@qq.com>
2019-08-24 22:19:02 +0800

    第一天

--- src/main.js
+++ src/main.js
@@ -1,11 +1,30 @@
 import Vue from 'vue'
 
+//1.1导入路由包
+import VueRouter from 'vue-router'
+//1.2安装路由模块
+Vue.use(VueRouter)
+
+//2.1导入vue-resource
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
+
+
 //引入mui的样式
 import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
 
-//引入header
-import { Header } from 'mint-ui';
+
+//引入header和轮播图
+import { Header, Swipe, SwipeItem} from 'mint-ui';
 Vue.component(Header.name, Header);
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
+
+
+
+//1.3导入自己的router.js模块
+import router from './router.js'
 
 
 //导入跟组件App.vue
@@ -13,5 +32,6 @@ import app from './App.vue'
 
 var vm = new Vue({
     el: '#app',
-    render: c =>c (app)
+    render: c =>c (app),
+    router  //1.4挂载路由
 })
--- src/router.js
+++ src/router.js
@@ -1,12 +1,22 @@
 import VueRouter from 'vue-router'
 
+import Home from './components/tabbar/Home.vue'
+import Member from './components/tabbar/Member.vue'
+import Shopcar from './components/tabbar/Shopcar.vue'
+import Search from './components/tabbar/Search.vue'
+
 
 var router = new VueRouter({
     routes: [
-
-    ]
+        {path:'/', redirect: '/home'},//路由重定向
+        {path:'/home', component: Home},
+        {path:'/member', component: Member},
+        {path:'/shopcar', component: Shopcar},
+        {path:'/search', component: Search}
+    ],
+    linkActiveClass: 'mui-active'//覆盖默认的路由高亮类，默认类router-link-active
 })
 
 
 
-exports.default = router
+export default router
