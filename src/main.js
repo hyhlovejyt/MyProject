a6c38208c45e504ef65844f53a52c07ae4018430
huangyuheng <1730969518@qq.com>
2019-08-26 03:01:29 +0800

    第二天

--- src/main.js
+++ src/main.js
@@ -5,9 +5,21 @@ import VueRouter from 'vue-router'
 //1.2安装路由模块
 Vue.use(VueRouter)
 
+
+// 导入格式化时间的插件
+import moment from 'moment'
+// 定义全局的过滤器
+Vue.filter('dateFormat', function (dataStr, pattern = "YYYY-MM-DD HH:mm:ss") {
+  return moment(dataStr).format(pattern)
+})
+
+
 //2.1导入vue-resource
 import VueResource from 'vue-resource'
 Vue.use(VueResource)
+
+//设置请求的根路径
+Vue.http.options.root = 'http://www.liulongbin.top:3005'
 
 
 //引入mui的样式
@@ -16,10 +28,11 @@ import './lib/mui/css/icons-extra.css'
 
 
 //引入header和轮播图
-import { Header, Swipe, SwipeItem} from 'mint-ui';
+import { Header, Swipe, SwipeItem, Button} from 'mint-ui';
 Vue.component(Header.name, Header);
 Vue.component(Swipe.name, Swipe);
 Vue.component(SwipeItem.name, SwipeItem);
+Vue.component(Button.name, Button);
 
 
 
--- src/router.js
+++ src/router.js
@@ -4,6 +4,8 @@ import Home from './components/tabbar/Home.vue'
 import Member from './components/tabbar/Member.vue'
 import Shopcar from './components/tabbar/Shopcar.vue'
 import Search from './components/tabbar/Search.vue'
+import NewsList from './components/news/NewsList.vue'
+import Newsinfo from './components/news/Newsinfo.vue'
 
 
 var router = new VueRouter({
@@ -12,7 +14,9 @@ var router = new VueRouter({
         {path:'/home', component: Home},
         {path:'/member', component: Member},
         {path:'/shopcar', component: Shopcar},
-        {path:'/search', component: Search}
+        {path:'/search', component: Search},
+        {path:'/home/newslist', component: NewsList},
+        {path:'/home/newsinfo/:id', component: Newsinfo},
     ],
     linkActiveClass: 'mui-active'//覆盖默认的路由高亮类，默认类router-link-active
 })
